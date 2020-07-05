0bd859d1e9f2ad1d91f47fb53061240c753c26a2
PhillipKinney <phillipjkinney@gmail.com>
2020-07-05 18:08:02 -0400

    completed 2nd exercise on slide 23

--- js/slide23.js
+++ js/slide23.js
@@ -15,6 +15,31 @@ function  smaller(){
     }
     }
     
-        
-
-     smaller();
+    function smaller3(){
+    
+        do{
+        usersThreeNumbers= prompt("Enter 3 numbers");
+        threeNumberArray= usersThreeNumbers.split(" ")
+        firstNumber= Number(threeNumberArray[0])
+        secondNumber=Number(threeNumberArray[1])
+        thirdNumber=Number(threeNumberArray[2])
+        }
+        while (isNaN(firstNumber) || isNaN(secondNumber) || isNaN(thirdNumber));
+    
+        if (firstNumber > secondNumber && thirdNumber > secondNumber)
+        {
+            document.write(secondNumber)
+        }
+        else if (secondNumber > firstNumber && thirdNumber > firstNumber)
+        { 
+            document.write(firstNumber);
+        }
+        else if (secondNumber > thirdNumber && firstNumber > thirdNumber)
+        {
+            document.write(thirdNumber)
+        }
+    }
+    
+      smaller3()      
+    
+         
